7c6eb5c0c0ed606e9dc053e1e9ea5d2dc2116fef
Alexis Bassignot <misterbalise2@gmail.com>
2023-10-03 14:10:54 +0200

    fin des tests

--- test/unitaire/adherant-test.php
+++ test/unitaire/adherant-test.php
@@ -9,12 +9,14 @@ require "vendor\autoload.php";
 // Fonction récuperée sur internet
 function isDigits($string): bool
 {
-    return !preg_match ("/[^0-9]/", $string);
+    return !preg_match("/[^0-9]/", $string);
 }
-function testNumeroAdherent (string $numeroAdherent) : bool {
+
+function testNumeroAdherent(string $numeroAdherent): bool
+{
     $ifTailleEgaleANeuf = strlen($numeroAdherent) == 9;
-    $ifCommenceParABTiret = str_starts_with($numeroAdherent, "AB-");
-    $ifContientNombreBienPlace = isDigits(substr($numeroAdherent,3));
+    $ifCommenceParABTiret = str_starts_with($numeroAdherent, "AD-");
+    $ifContientNombreBienPlace = isDigits(substr($numeroAdherent, 3));
     if ($ifTailleEgaleANeuf && $ifCommenceParABTiret && $ifContientNombreBienPlace) {
         return true;
     }
@@ -32,7 +34,7 @@ echo ESC;
 $textTestDateNonDefinie = "Test: vérifier que la date d’adhésion, si non précisée à la création, est égale à la date du jour";
 
 // Arrange
-$adherentDateNonDefinie = new Adherent("Almiche","Ganache", "test@test.com");
+$adherentDateNonDefinie = new Adherent("Almiche", "Ganache", "test@test.com");
 $valeurAttendueTestDateNonDefinie = (new DateTime())->format("d/m/Y");
 
 // Act
@@ -45,7 +47,7 @@ assertTestUnitaire(
     $valeurAttendueTestDateNonDefinie
 );
 
-// vérifier que la date d’adhésion, si précisée à la création, est bien prise en compte"
+// vérifier que la date d’adhésion, si précisée à la création, est bien prise en compte
 $textTestDateDefinie = "Test: vérifier que la date d’adhésion, si précisée à la création, est bien prise en compte";
 // Arrange
 $dateDefinie = "05/11/2022";
@@ -62,45 +64,72 @@ assertTestUnitaire(
 );
 
 
+// vérifier que le numéro d’adhérent, à la création, est valide
+$textTestNombreValide = "Test: vérifier que le numéro d’adhérent, à la création, est valide";
+// Arrange
+$adherentNombreValide = new Adherent("Jamie",
+    "Sacripan", "test@test.com",);
+$valeurAttendueTestNombreValide = true;
+// Act
+$valeurEntreeTestNombreValide = testNumeroAdherent($adherentNombreValide->getNumeroadherent());
+// Assertion
+assertTestUnitaire(
+    $textTestNombreValide,
+    $valeurEntreeTestNombreValide,
+    $valeurAttendueTestNombreValide
+);
 
 
-// Test numéro 3
-$textTest3 = "On regarde si l'abonnement est toujours valide";
+// vérifier  que  l’adhésion est valable  (valide)  quand  la  date d’adhésion n’est pas dépassée (moins d’un an)
+$textTestEncours = "Test: vérifier  que  l’adhésion est valable  (valide)  quand  la  date d’adhésion n’est pas dépassée (moins d’un an)";
 // Arrange
 $adherentEncours = new Adherent("Jack",
     "Trompette", "test@test.com");
 $adherentEncours->getDateAdhesion()->modify("-6month");
-$valeurAttendueTest3 = true;
+$valeurAttendueTestEncours = true;
 // Act
-$valeurEntreeTest3 = $adherentEncours->ifAbonnementEnCours();
+$valeurEntreeTestEncours = $adherentEncours->ifAbonnementEnCours();
 // Assert
 assertTestUnitaire(
-    $textTest3,
-    $valeurEntreeTest3,
-    $valeurAttendueTest3
+    $textTestEncours,
+    $valeurEntreeTestEncours,
+    $valeurAttendueTestEncours
 );
 
 
-// Test numéro 4
-$textTest4 = "On renouvelle pour 1 an l'abonnement";
+// vérifier  que  l’adhésion est non  valable  (invalide)  quand  la  date d’adhésion est dépassée (plus d’un an)
+$textTestPasEncours = "Test: vérifier  que  l’adhésion est non  valable  (invalide)  quand  la  date d’adhésion est dépassée (plus d’un an)";
+// Arrange
+$adherentPasEncours = new Adherent("Jack",
+    "Trompette", "test@test.com");
+$adherentPasEncours->getDateAdhesion()->modify("-14month");
+$valeurAttendueTestPasEncours = false;
+// Act
+$valeurEntreeTestPasEncours = $adherentPasEncours->ifAbonnementEnCours();
+// Assert
+assertTestUnitaire(
+    $textTestPasEncours,
+    $valeurEntreeTestPasEncours,
+    $valeurAttendueTestPasEncours
+);
+
+
+// valable  (valide)  quand  la  date d’adhésion n’est pas dépassée (moins d’un an)
+$textTestRenouvellement = "Test valable  (valide)  quand  la  date d’adhésion n’est pas dépassée (moins d’un an)";
 // Arrange
 $dateAdhestion = "05/05/2023";
-$adherentEncours = new Adherent("James",
+$adherentRenouvellement = new Adherent("James",
     "Chapelin", "test@test.com", $dateAdhestion);
-$valeurAttendueTest4 =
+$valeurAttendueTestRenouvellement =
     \DateTime::createFromFormat("d/m/Y", $dateAdhestion)
         ->modify("+1year")
         ->format("d/m/Y");
 // Act
-$adherentEncours->renouvelerAdhesion();
-$valeurEntreeTest4 = $adherentEncours->getDateAdhesionToString();
+$adherentRenouvellement->renouvelerAdhesion();
+$valeurEntreeTestRenouvellement = $adherentRenouvellement->getDateAdhesionToString();
 // Assert
 assertTestUnitaire(
-    $textTest4,
-    $valeurEntreeTest4,
-    $valeurAttendueTest4
+    $textTestRenouvellement,
+    $valeurEntreeTestRenouvellement,
+    $valeurAttendueTestRenouvellement
 );
-
-
-//dump($adherentDateDefinie);
-//dump($adherentDatenonDefinie);
